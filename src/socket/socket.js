ef5b7ada46b9ace4cd2e20a2c3473f0a3c34748e
DhiaZahmoul <zahmouldhia@gmail.com>
2025-11-29 19:21:55 +0100

    socket fix

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -3,4 +3,4 @@
 //Might get modified later as integration improves
 import { io } from 'socket.io-client';
 
-export const socket = io('${process.env.NEXT_PUBLIC_API_URL}', { transports: ['websocket'] });
+export const socket = io(`${process.env.NEXT_PUBLIC_API_URL}`, { transports: ['websocket'] });
